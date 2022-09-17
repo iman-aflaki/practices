a9895b5208d52e3b1467b2f5bd11c408ee84a232
iman aflaki <113721468+iman-aflaki@users.noreply.github.com>
2022-09-17 15:45:30 +0430

    Update main.py
    accept an even number from the user and create a combinations that express the given number as a sum of two prime numbers.

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-#.....return true if number is prime
 def is_prime(n):
-  for i in range(2,n):
-    if (n%i) == 0:
-      return False
-  return True
+    for i in range(2, n):
+        if (n % i) == 0:
+            return False
+    return True
+
 
 exit_or_not = ''
 while exit_or_not != 'y':
     in_num = int(input("enter any even number ..."))
-    for index in range(1,int((in_num)/2)+1):
+    for index in range(1, int(in_num/2)+1):
         if is_prime(index):
             sep = in_num - index
             if is_prime(sep):
-                print("%d + %d = %d"%(index,sep,in_num))
-    exit_or_not = input("exite? y/n")
+                print("%d + %d = %d" % (index, sep, in_num))
+    exit_or_not = input("exit? y/n")
     if exit_or_not == 'y':
         break
+        
